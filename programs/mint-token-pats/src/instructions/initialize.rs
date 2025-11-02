c73e06dfc12e037c8cf43101962e358728c26fae
PatoAh <98765285+vilmapato@users.noreply.github.com>
2025-11-02 03:05:42 +0100

    added image token

--- programs/mint-token-pats/src/instructions/initialize.rs
+++ programs/mint-token-pats/src/instructions/initialize.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_2022::{self, initialize_mint2, InitializeMint2, spl_token_2022::{extension::ExtensionType, pod::PodMint} };
+use anchor_spl::{token_2022::{self, InitializeMint2, update_metadata, spl_token_2022::{extension::ExtensionType, pod::PodMint} }, token_interface::{MetadataPointerInitialize, token_metadata}};
 
 
 pub fn initialize_mint(ctx: Context<InitializeMintSimple>)-> Result<()> {
@@ -10,13 +10,35 @@ pub fn initialize_mint(ctx: Context<InitializeMintSimple>)-> Result<()> {
     
     //init mint
     let mint_ctx = CpiContext::new(
-        ctx.accounts.token_program.to_account_info(),
+        token_program.to_account_info(),
         InitializeMint2 {
-            mint: ctx.accounts.mint.to_account_info(),
+            mint: mint.to_account_info(),
 
         },
+    ); 
+    let meta_ptr_ctx = CpiContext::new(
+        token_program.to_account_info(),
+        MetadataPointerInitialize {
+            mint: mint.to_account_info(),
+            token_program_id: token_program.to_account_info(),
+        },
     );
-    initialize_mint2(cpi_ctx, 6, &ctx.accounts.creator.key(), None,)?;
+
+    let update_meta_ctx = CpiContext::new(
+        token_program.to_account_info(),
+        UpdateMetadata {
+            token_program.to_account_info(),
+            mint: mint.to_account_info(),
+        },
+    );
+
+    update_metadata(
+        update_meta_ctx,
+        "PATS Token".to_string(),
+        "PATS".to_string(),
+        "https://raw.githubusercontent.com/vilmapato/mint-token-pats/main/metadata.json".to_string(),
+    )?;
+    msg!("✅ Metadata successfully set for PATS Token!");
     Ok(())
 }
 
